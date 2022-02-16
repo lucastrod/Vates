f5046f036d7f3cca7eadf9eed5bd8c411464a539
lucastrod <castro.ld@outlook.com>
2022-02-16 18:46:45 -0300

    Add project files.

---
+++ ConsoleApp3/Pages/LoginPage.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VatesPruebaTecnica.Pages
+{
+    class LoginPage
+    {
+        private IWebDriver webDriver;
+
+        public LoginPage(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+        }
+
+        private IWebElement inpUsername => webDriver.FindElement(By.Id("user-name"));
+
+        private IWebElement inpPassword => webDriver.FindElement(By.Id("password"));
+
+        private IWebElement btnLogin => webDriver.FindElement(By.Id("login-button"));
+
+
+        public void Login(string username, string password)
+        {
+            webDriver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+            inpUsername.SendKeys(username);
+
+            inpPassword.SendKeys(password);
+
+            btnLogin.Click();
+
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VatesPruebaTecnica.Pages;
+
+namespace VatesPruebaTecnica
+{
+    class Program
+    {
+        static IWebDriver LaunchBrowser()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            options.AddArgument("--disable-notifications");
+            options.AddExcludedArgument("enable-automation");
+            var chromeDriverService = ChromeDriverService.CreateDefaultService(Environment.CurrentDirectory);
+            chromeDriverService.HideCommandPromptWindow = true;
+
+            var driver = new ChromeDriver(chromeDriverService, options);
+            return driver;
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("User Login");
+            var webDriver = LaunchBrowser();
+            try
+            {
+                var start = new LoginPage(webDriver);
+                Console.WriteLine("-------------------------------------------------------------------------");
+                Console.WriteLine("Login...");
+                #region Parameters
+                var user = "standard_user";
+                var pw = "secret_sauce";
+                #endregion
+                start.Login(user, pw);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while executing automation");
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                webDriver.Quit();
+                Console.WriteLine("Login Succesfully");
+                Console.ReadKey();
+            }
+        }
+    }
+}
